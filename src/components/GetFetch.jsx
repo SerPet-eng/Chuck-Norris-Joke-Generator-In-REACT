732e72255755400ddc8890c00637fcb34e45e052
SerPet-eng <62647434+SerPet-eng@users.noreply.github.com>
2023-09-04 21:44:03 +0800

    Second Commit / Update GetFetch
    9:38 PM
    9/4/2023
    
    - I update the fetching part of the src/components/GetFetch.jsx
    
      * Async / Await
      * Try / Catch
        - For catching errors
      * Promise.all / Array.from
       - For iteration
      * Changing nextJoke()
      * Add some variables:
        - maxRequestPerHour
        - requestInterval
        - requestCount

--- src/components/GetFetch.jsx
+++ src/components/GetFetch.jsx
@@ -4,21 +4,40 @@ function JokeGenerator() {
     const [jokes, setJokes] = useState(['Welcome to Chuck Norris Joke Generator! Come on, have a little smile by reading and clicking the button below'])
     const [currentJokeIndex, setCurrentJokeIndex] = useState(0)
 
+    const jokeURL = 'https://api.chucknorris.io/jokes/random'
+    const maxRequestPerHour = 50
+    const requestInterval = 720000
+    let requestCount = 0
+
     useEffect(() => {
-        const fetchJokes = async () => {
-          const jokeURL = 'https://api.chucknorris.io/jokes/random';
-          const response = await fetch(jokeURL);
-          const data = await response.json();
-          setJokes([...jokes, data.value]);
-        };
-    
-        // Fetch jokes when the component mounts
-        fetchJokes();
-      }, []);
+        async function fetchJoke() {
+            try {
+                const responses = await Promise.all(Array.from({ 
+                    length: maxRequestPerHour },
+                    () => fetch(jokeURL))
+                )
+                const jsonData = await Promise.all(responses.map(response => response.json()))
+                setJokes(jsonData.map(data => data.value))
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchJoke()
+      }, [requestCount]);
 
     const nextJoke = () => {
-        setCurrentJokeIndex((currentJokeIndex + 1) % jokes.length)
+        if(requestCount < maxRequestPerHour) {
+            setCurrentJokeIndex(index => (index + 1) % jokes.length)
+            requestCount++
+        } else {
+            console.log('Maximum number of request reached')
+        }
     }
+
+    setInterval( () => {
+        requestCount = 0
+    }, requestInterval)
 
     return (
         <>
